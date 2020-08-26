4c3a47c44e65dc27be29ff5ab05e63fa9f5e4e55
SaurabhS78 <saurabhsangam78@gmail.com>
2020-08-26 22:17:00 +0530

    Updates

---
+++ precache-manifest.76733b82225063f5b71bc08b613a8d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17b68a87b963ec07cecee897c6066c7",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "d94343094eb9195240a5",
+    "url": "/react-recipe-app/static/css/main.b971ce07.chunk.css"
+  },
+  {
+    "revision": "a0a66ddad20475ff86dc",
+    "url": "/react-recipe-app/static/js/2.2fe0a489.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-recipe-app/static/js/2.2fe0a489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94343094eb9195240a5",
+    "url": "/react-recipe-app/static/js/main.4ca6f964.chunk.js"
+  },
+  {
+    "revision": "1eb68c2075f549be9c1b",
+    "url": "/react-recipe-app/static/js/runtime-main.1447bd45.js"
+  }
+]);
